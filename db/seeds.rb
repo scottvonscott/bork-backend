d4ff4c08e031dfd8e5cb3e5c80dfb9315f070b90
scottvonscott <skeplin17@gmail.com>
2020-09-23 14:29:40 -0700

    img srcs for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,19 +22,19 @@ entry_text: "The third level seems to glow with it's own unearthly light, the ca
 exit_text: "You know the way out and nothing can stop you!"
 )
 
-Monster.create(name: "Giant Rat", health: 10, attack: 5, dungeon_level_id: 1, img_url: "",
+Monster.create(name: "Giant Rat", health: 10, attack: 5, dungeon_level_id: 1, img_url: "https://www.worldanvil.com/uploads/images/4bc0da1239af04560ef87f173083429e.jpg",
 intro_text: "A Giant Rat scurries around the corner, chittering furiously.",
 quote: "You'll brie sorry you crossed me!",
 death_text: "The Giant Rat falls over dead. A few items fall out his bag and you help yourself. You unfortunately can't think of a cheese pun.",
 )
 
-Monster.create(name: "Minotaur", health: 15, attack: 8, dungeon_level_id: 2, img_url: "",
+Monster.create(name: "Minotaur", health: 15, attack: 8, dungeon_level_id: 2, img_url: "https://gamertweak.com/wp-content/uploads/2020/05/minotaur-assassins-creed-odyssey.jpg",
 intro_text: "A huge Minotaur runs up and snorts heavily. He flexes his muscles for you",
 quote: "I'm the beefiest!",
 death_text: "The Minotaur falls over dead, knocking over a chest of items. Cow punnnnn",
 )
 
-Monster.create(name: "Dragon", health: 30, attack: 15, dungeon_level_id: 3, img_url: "",
+Monster.create(name: "Dragon", health: 30, attack: 15, dungeon_level_id: 3, img_url: "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/12/9/1418121741203/Smaug-011.jpg?width=300&quality=45&auto=format&fit=max&dpr=2&s=da5839aacac848a16bff061cdf73d3e9",
 intro_text: "An enormous Dragon sits upon a huge pile of treasure. She roars and breathes fire into the air.",
 quote: "Giant piles of treasure offer very poor lumbar support.",
 death_text: "The Dragon falls over dead.",
